ae24177e1cac30fdfc47234ab8c3a75b2361414e
Jesse Millar <hellojessemillar@gmail.com>
2016-03-17 14:24:22 -0600

    Working script

---
+++ Tools/mass-usp-compiler/mass-usp-compiler.ps1
@@ -0,0 +1,10 @@
+﻿$Source = Split-Path -Path $PSScriptRoot -Parent
+$FolderPath = Split-Path -Path $Source -Parent
+$FileExtension = ".usp"
+
+$Dir = get-childitem $FolderPath -recurse
+$List = $Dir | where {$_.extension -eq $FileExtension}
+
+$List | ForEach-Object {
+    & "C:\Program Files (x86)\Crestron\Simpl\SPlusCC.exe" \build $_.FullName \target series2 series3
+}
